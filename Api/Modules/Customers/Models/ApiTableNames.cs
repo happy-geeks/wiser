eaccdd083c855ec53154d0652ba340b0996f0528
Gilian <giliankeulens@happyhorizon.com>
2022-01-11 13:53:19 +0100

    Fixed problem that logging in as admin account did not work properly yet, because I forgot to change ValidateAdminAccountIdAsync before.

--- Api/Modules/Customers/Models/ApiTableNames.cs
+++ Api/Modules/Customers/Models/ApiTableNames.cs
@@ -2,8 +2,6 @@
 {
     public class ApiTableNames
     {
-        // Wiser tables
         public const string WiserCustomers = "easy_customers";
-        public const string WiserAdminAccounts = "admin_accounts";
     }
 }
