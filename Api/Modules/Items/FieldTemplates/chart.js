671072c8553a0762f11cbe1d89b4968872568ada
Gilian <giliankeulens@happyhorizon.com>
2022-01-12 11:56:32 +0100

    Fixed issues with the chart component
    - We did not send the correct content type with the request to the API.
    - Setting the height in wiser_entityproperty didn't do anything.

--- Api/Modules/Items/FieldTemplates/chart.js
+++ Api/Modules/Items/FieldTemplates/chart.js
@@ -2,9 +2,17 @@
     var field = $("#chart{propertyIdWithSuffix}");
     var loader = field.closest(".item").find(".field-loader");
     var optionsFromProperty = {options};
+    var height = parseInt("{height}") || undefined;
+    var chartArea;
+    if (height) {
+        chartArea = {
+            height: height
+        }
+    }
 
     var options = $.extend(true, {
         title: { text: "{title}" },
+        chartArea: chartArea,
         dataSource: {
             transport: {
                 read: (options) => {
@@ -12,6 +20,7 @@
                         url: dynamicItems.settings.wiserApiRoot + "items/" + encodeURIComponent("{itemIdEncrypted}") + "/action-button/{propertyId}?queryId=" + encodeURIComponent(optionsFromProperty.queryId || 0),
                         dataType: "json",
                         method: "POST",
+                        contentType: "application/json",
                         data: options.data
                     }).then((result) => {
                         options.success(result);
@@ -29,10 +38,8 @@
             loader.removeClass("loading");
         }
     }, optionsFromProperty);
-    console.log("chart options", options);
 
     var kendoComponent = field.kendoChart(options).data("kendoChart");
-    console.log("chart", kendoComponent);
 	
     {customScript}
 })();
