efacd69022840633ffcd0e4ebdec8f0d839b5356
Gilian <giliankeulens@happyhorizon.com>
2022-02-10 11:42:47 +0100

    Refactored some code for DynamicContent, we now use proper view models for it.

---
+++ FrontEnd/Modules/Templates/Models/FieldViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using GeeksCoreLibrary.Core.Cms.Attributes;
+
+namespace FrontEnd.Modules.Templates.Models
+{
+    public class FieldViewModel
+    {
+        public string Name { get; set; }
+
+        public object Value { get; set; }
+
+        public PropertyInfo PropertyInfo { get; set; }
+
+        public CmsPropertyAttribute CmsPropertyAttribute { get; set; }
+    }
+}
---
+++ FrontEnd/Modules/Templates/Models/TabViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GeeksCoreLibrary.Core.Cms.Attributes;
+
+namespace FrontEnd.Modules.Templates.Models
+{
+    public class TabViewModel
+    {
+        public CmsAttributes.CmsTabName Name { get; set; }
+
+        public string PrettyName { get; set; }
+
+        public List<GroupViewModel> Groups { get; set; } = new();
+    }
+}
