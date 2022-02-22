2a8fa4fe74416f4dce3f3d2c392ab78a333b20af
Gilian <giliankeulens@happyhorizon.com>
2022-02-22 14:29:11 +0100

    Sub-entities-grid - Fixed bug caused by camelCase refactor, that you could no longer open items in some grids.

--- Api/Modules/Items/FieldTemplates/sub-entities-grid.js
+++ Api/Modules/Items/FieldTemplates/sub-entities-grid.js
@@ -39,7 +39,7 @@ if (customQueryGrid) {
         if (!options.disableOpeningOfItems) {
             if (customQueryResults.schemaModel && customQueryResults.schemaModel.fields) {
                 // If there is no field for encrypted ID, don't allow the user to open items, they'd just get an error.
-                options.disableOpeningOfItems = !(customQueryResults.schemaModel.fields.encryptedId || customQueryResults.schemaModel.fields.encryptedId || customQueryResults.schemaModel.fields.encryptedid || customQueryResults.schemaModel.fields.idencrypted);
+                options.disableOpeningOfItems = !(customQueryResults.schemaModel.fields.encryptedId || customQueryResults.schemaModel.fields.encrypted_id || customQueryResults.schemaModel.fields.encryptedid || customQueryResults.schemaModel.fields.idencrypted);
             }
         }
         
@@ -140,7 +140,7 @@ if (customQueryGrid) {
         if (!options.disableOpeningOfItems) {
             if (gridSettings.schemaModel && gridSettings.schemaModel.fields) {
                 // If there is no field for encrypted ID, don't allow the user to open items, they'd just get an error.
-                options.disableOpeningOfItems = !(gridSettings.schemaModel.fields.encryptedId || gridSettings.schemaModel.fields.encryptedId || gridSettings.schemaModel.fields.encryptedid || gridSettings.schemaModel.fields.idencrypted);
+                options.disableOpeningOfItems = !(gridSettings.schemaModel.fields.encryptedId || gridSettings.schemaModel.fields.encrypted_id || gridSettings.schemaModel.fields.encryptedid || gridSettings.schemaModel.fields.idencrypted);
             }
         }
         
