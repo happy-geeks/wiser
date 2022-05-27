1a78ae564ba6cb3746b8b7ca7de157b0903871c6
Gilian <giliankeulens@happyhorizon.com>
2022-05-27 09:45:40 +0200

    Updating to .NET 6

--- Api/Modules/Barcodes/Controllers/BarcodesController.cs
+++ Api/Modules/Barcodes/Controllers/BarcodesController.cs
@@ -31,6 +31,7 @@ namespace Api.Modules.Barcodes.Controllers
             }
 
             using var qrGenerator = new QRCodeGenerator();
+            
             using var qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
             using var qrCode = new QRCode(qrCodeData);
             using var qrCodeImage = new Bitmap(qrCode.GetGraphic(20), new Size(size, size));
