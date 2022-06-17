719f7f57b88ad957291b24b106029000255d0340
Bram <414295@student.fontys.nl>
2022-06-17 16:29:21 +0200

    Added some grid functions

---
+++ Api/Modules/Kendo/Models/ModuleGridDataSettings.cs
@@ -0,0 +1,31 @@
+﻿namespace Api.Modules.Kendo.Models
+{    
+    /// <summary>
+    /// A model for the settings of a grid
+    /// </summary>
+    public class ModuleGridDataSettings
+    {
+        /// <summary>
+        /// The query to get the data of the grid
+        /// </summary>
+        public string CustomQuery { get; set; }
+
+        /// <summary>
+        /// The count query for the grid
+        /// </summary>
+        public string CountQuery { get; set; }
+
+        /// <summary>
+        /// The options for the grid
+        /// </summary>
+        public string GridOptions { get; set; }
+
+        /// <summary>
+        /// The read options of the grid
+        /// </summary>
+        public GridReadOptionsModel GridReadOptions { get; set; }
+
+
+    }
+}
+
