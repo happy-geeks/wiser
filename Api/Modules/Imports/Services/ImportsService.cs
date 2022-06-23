ba014ef61a4b1d43e2f59375319fe68bfbb0ddcd
Hans <35447467+HansJuice@users.noreply.github.com>
2022-06-23 13:36:10 +0200

    Updated import module entity properties (#91)
    * Updated import module entity properties
    
    Entity properties are now selected base on their order in Wiser instead of always alphabetically. The list is still ordered alphabetically though. Also refactored the 'get-entity-properties' call to use an endpoint of the Imports module instead of using the old query in the templates service.
    
    * Updated entity property model usage
    
    Removed custom EntityPropertyModel class from the Imports module and updated code to use the EntityPropertyModel that is available in the EntityProperties module.

--- Api/Modules/Imports/Services/ImportsService.cs
+++ Api/Modules/Imports/Services/ImportsService.cs
@@ -20,6 +20,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Api.Core.Enums;
 using Api.Modules.Customers.Models;
+using Api.Modules.EntityProperties.Helpers;
+using Api.Modules.EntityProperties.Models;
 using GeeksCoreLibrary.Core.Extensions;
 using GeeksCoreLibrary.Core.Helpers;
 using GeeksCoreLibrary.Core.Interfaces;
@@ -1258,6 +1260,74 @@ AND destinationDetail.`value` IN({String.Join(",", destinationValues.Select(line
                 await clientDatabaseConnection.RollbackTransactionAsync(false);
                 throw;
             }
+        }
+
+        /// <inheritdoc />
+        public async Task<ServiceResult<IEnumerable<EntityPropertyModel>>> GetEntityProperties(ClaimsIdentity identity, string entityName = null, int linkType = 0)
+        {
+            await clientDatabaseConnection.EnsureOpenConnectionForReadingAsync();
+            
+            clientDatabaseConnection.ClearParameters();
+            clientDatabaseConnection.AddParameter("entityName", entityName ?? String.Empty);
+            clientDatabaseConnection.AddParameter("linkType", linkType);
+
+            var getPropertiesResult = await clientDatabaseConnection.GetAsync($@"
+                SELECT property.id, property.display_name, property.property_name, property.language_code, property.inputtype, property.`options`, property.ordering
+                FROM (
+                    SELECT
+                        0 AS id,
+                        'Item naam' AS display_name,
+                        'itemTitle' AS property_name,
+                        '' AS language_code,
+                        'input' AS inputtype,
+                        '' AS `options`,
+                        1 AS ordering,
+                        0 AS base_order
+                    FROM DUAL
+                    WHERE ?entityName <> ''
+                    UNION
+                    SELECT
+                        id,
+                        CONCAT(
+                            IF(display_name = '', property_name, display_name),
+                            IF(
+                                language_code <> '',
+                                CONCAT(' (', language_code, ')'),
+                                ''
+                            )
+                        ) AS display_name,
+                        IF(property_name = '', display_name, property_name) AS property_name,
+                        language_code,
+                        inputtype,
+                        IF(inputtype = 'image-upload', `options`, '') AS `options`,
+                        ordering AS ordering,
+                        1 AS base_order
+                    FROM `{WiserTableNames.WiserEntityProperty}`
+                    WHERE entity_name = ?entityName OR (?linkType > 0 AND link_type = ?linkType)
+                    ORDER BY base_order, display_name
+                ) AS property");
+
+            if (getPropertiesResult.Rows.Count == 0)
+            {
+                return new ServiceResult<IEnumerable<EntityPropertyModel>>(null);
+            }
+
+            var entityProperties = new List<EntityPropertyModel>(getPropertiesResult.Rows.Count);
+            foreach (var entityPropertyDataRow in getPropertiesResult.Rows.Cast<DataRow>())
+            {
+                entityProperties.Add(new EntityPropertyModel
+                {
+                    Id = Convert.ToInt32(entityPropertyDataRow["id"]),
+                    DisplayName = entityPropertyDataRow.Field<string>("display_name"),
+                    PropertyName = entityPropertyDataRow.Field<string>("property_name"),
+                    LanguageCode = entityPropertyDataRow.Field<string>("language_code"),
+                    InputType = EntityPropertyHelper.ToInputType(entityPropertyDataRow.Field<string>("inputtype")),
+                    Options = entityPropertyDataRow.Field<string>("options") ?? String.Empty,
+                    Ordering = Convert.ToInt32(entityPropertyDataRow["ordering"])
+                });
+            }
+
+            return new ServiceResult<IEnumerable<EntityPropertyModel>>(entityProperties);
         }
     }
 }
