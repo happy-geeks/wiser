c52f81498476f6c30079b8c9381078e7caba700f
DavevandeV <davevandeven@happyhorizon.com>
2022-07-07 15:38:51 +0200

    Image-upload.js - merged two SaveURL calls

--- Api/Modules/Items/FieldTemplates/image-upload.js
+++ Api/Modules/Items/FieldTemplates/image-upload.js
@@ -14,8 +14,7 @@ var options = {options};
 
 options = $.extend({
     async: {
-        saveUrl: window.dynamicItems.settings.wiserApiRoot + "items/{itemIdEncrypted}/upload?propertyName=" + encodeURIComponent("{propertyName}") + "&itemLinkId={itemLinkId}&useTinyPng=" + (options.useTinyPng === true).toString() + "&entityType=" + encodeURIComponent("{entityType}") + "&linkType={linkType}",
-        saveUrl: window.dynamicItems.settings.wiserApiRoot + "items/{itemIdEncrypted}/upload?propertyName=" + encodeURIComponent("{propertyName}") + "&itemLinkId={itemLinkId}&useTinyPng=" + (options.useTinyPng === true).toString() + "&useCloudFlare=" + (options.useCloudFlare === true).toString(),
+        saveUrl: window.dynamicItems.settings.wiserApiRoot + "items/{itemIdEncrypted}/upload?propertyName=" + encodeURIComponent("{propertyName}") + "&itemLinkId={itemLinkId}&useTinyPng=" + (options.useTinyPng === true).toString() + "&useCloudFlare=" + (options.useCloudFlare === true).toString() + "&entityType=" + encodeURIComponent("{entityType}") + "&linkType={linkType}",
         withCredentials: false,
         removeUrl: "remove"
     },
