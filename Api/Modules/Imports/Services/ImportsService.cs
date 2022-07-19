78f7e26c904bf72f894aa10909686c1b91da6c78
Gilian <giliankeulens@happyhorizon.com>
2022-07-19 16:01:59 +0200

    Import - Fixed bug that grids were initialised twice, which in turn caused a lot of other problems.

--- Api/Modules/Imports/Services/ImportsService.cs
+++ Api/Modules/Imports/Services/ImportsService.cs
@@ -567,7 +567,7 @@ namespace Api.Modules.Imports.Services
                 clientDatabaseConnection.AddParameter("data", json);
                 clientDatabaseConnection.AddParameter("server_name", Environment.MachineName);
                 clientDatabaseConnection.AddParameter("sub_domain", subDomain);
-                wiserImportId = await clientDatabaseConnection.InsertOrUpdateRecordBasedOnParametersAsync<int>("wiser_import");
+                wiserImportId = await clientDatabaseConnection.InsertOrUpdateRecordBasedOnParametersAsync<int>(WiserTableNames.WiserImport);
             }
             catch (Exception ex)
             {
@@ -585,7 +585,7 @@ namespace Api.Modules.Imports.Services
                 clientDatabaseConnection.AddParameter("errors", String.Join(Environment.NewLine, importResult.Errors));
                 clientDatabaseConnection.AddParameter("added_on", DateTime.Now);
                 clientDatabaseConnection.AddParameter("added_by", IdentityHelpers.GetUserName(identity));
-                await clientDatabaseConnection.InsertOrUpdateRecordBasedOnParametersAsync<int>("wiser_import_log");
+                await clientDatabaseConnection.InsertOrUpdateRecordBasedOnParametersAsync<int>(WiserTableNames.WiserImportLog);
             }
             catch (Exception exception)
             {
