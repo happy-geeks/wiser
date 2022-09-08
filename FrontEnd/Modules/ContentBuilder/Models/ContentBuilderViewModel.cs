35fa9be965caea639de940f07f1affd33cda9a8a
Gilian <giliankeulens@happyhorizon.com>
2022-09-08 15:34:15 +0200

    ContentBuilder will now load the same CSS as HTML editors.

---
+++ FrontEnd/Modules/ContentBuilder/Models/ContentBuilderViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FrontEnd.Core.Models;
+
+namespace FrontEnd.Modules.ContentBuilder.Models;
+
+public class ContentBuilderViewModel : BaseViewModel
+{
+    /// <summary>
+    /// Gets or sets the encrypted user ID.
+    /// </summary>
+    public string UserId { get; set; }
+}
