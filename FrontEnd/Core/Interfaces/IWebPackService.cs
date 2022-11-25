7fc61ff3a4b70ce8ba0109845c8b4834282491e6
Gilian <giliankeulens@happyhorizon.com>
2022-11-25 15:59:44 +0100

    Added content hash to all webpack files and a manifest
    This is to fix the problem that users had to clear their browser cache after every Wiser update.

---
+++ FrontEnd/Core/Interfaces/IWebPackService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace FrontEnd.Core.Interfaces;
+
+public interface IWebPackService
+{
+    /// <summary>
+    /// Load the webpack manifest file into memory.
+    /// </summary>
+    Task InitializeAsync();
+    
+    /// <summary>
+    /// Gets the full filename and location for a file from the manifest.
+    /// </summary>
+    /// <param name="fileName">The name of the file to get the location for.</param>
+    /// <returns>The full location to the file, including path and file name.</returns>
+    string GetManifestFile(string fileName);
+}
