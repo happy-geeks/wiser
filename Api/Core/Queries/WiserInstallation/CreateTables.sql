9cd8a1ecbc37f34975f56426964efe9b4623c10f
MarkVanDijkHG <89684687+MarkVanDijkHG@users.noreply.github.com>
2023-01-10 12:04:50 +0100

    Add is_partial to initial creation of tables and move location in Wiser. (#216)
    "is_partial" column will now be added to the table during creation of a new customer.
    
    The option in the settings is moved to a more logical location.

--- Api/Core/Queries/WiserInstallation/CreateTables.sql
+++ Api/Core/Queries/WiserInstallation/CreateTables.sql
@@ -705,6 +705,7 @@ CREATE TABLE IF NOT EXISTS `wiser_template`  (
    `is_default_header` tinyint(1) NOT NULL DEFAULT 0,
    `is_default_footer` tinyint(1) NOT NULL DEFAULT 0,
    `default_header_footer_regex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+   `is_partial` tinyint(1) NOT NULL DEFAULT 0,
    PRIMARY KEY (`id`) USING BTREE,
    UNIQUE INDEX `idx_unique`(`template_id` ASC, `version` ASC) USING BTREE,
    INDEX `idx_removed`(`removed` ASC) USING BTREE,
