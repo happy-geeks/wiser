ecc5ee3fae02b189b55aeff4daaaf76b87d88a00
JeremyMullerr <112623887+JeremyMullerr@users.noreply.github.com>
2023-01-12 14:34:17 +0100

    Added check on reserved values in Javascript (#206)
    * Added check on reserved values in Javascript
    
    * ReservedWordsArray is now a var array and string interpolation used

--- Api/Modules/Grids/Services/GridsService.cs
+++ Api/Modules/Grids/Services/GridsService.cs
@@ -894,12 +894,17 @@ namespace Api.Modules.Grids.Services
                     clientDatabaseConnection.AddParameter("entityType", entityType);
                     clientDatabaseConnection.AddParameter("linkTypeNumber", linkTypeNumber);
                     var columnsDataTable = await clientDatabaseConnection.GetAsync(columnsQuery);
-
+                    var reservedWordsArray = new[] { "abstract","arguments","await","boolean","break","byte","case","catch","char","class","const","continue","debugger","default","delete","do","double","else","enum","eval","export","extends","false","final","finally","float","for","function","goto","if","implements","import","in","instanceof","int","interface","let","long","native","new","null","package","private","protected","public","return","short","static","super","switch","synchronized","this","throw","throws","transient","true","try","typeof","var","void","volatile","while","with","yield" };
+                    
                     if (columnsDataTable.Rows.Count > 0)
                     {
                         foreach (DataRow dataRow in columnsDataTable.Rows)
                         {
                             var fieldName = dataRow.Field<string>("field").ToLowerInvariant().MakeJsonPropertyName();
+                            if (reservedWordsArray.Contains(fieldName))
+                            {
+                                throw new Exception( $"{fieldName}(variable: fieldName) is a reserved Javascript keyword");
+                            }
 
                             var field = new FieldModel
                             {
