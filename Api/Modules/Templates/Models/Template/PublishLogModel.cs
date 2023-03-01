608b3b3c994efa88a11699604ed2109d755bdb32
Gilian <giliankeulens@happyhorizon.com>
2023-03-01 14:32:52 +0100

    Fixed compilation error.

--- Api/Modules/Templates/Models/Template/PublishLogModel.cs
+++ Api/Modules/Templates/Models/Template/PublishLogModel.cs
@@ -48,13 +48,6 @@
         }
 
         /// <summary>
-        /// Creates a new instance of <see cref="PublishLogModel"/>.
-        /// </summary>
-        public PublishLogModel()
-        {
-        }
-
-        /// <summary>
         /// Create a PublishLogModel with all values set to a base. Will set the old environments as new values.
         /// </summary>
         /// <param name="id">The id of the template</param>
