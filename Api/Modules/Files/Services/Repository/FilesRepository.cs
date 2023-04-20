891c9ba1ad98311f6d573c9fdb43edd7b255dc2c
Gilian <giliankeulens@happyhorizon.com>
2023-04-20 09:30:45 +0200

    Converted all indents to spaces.

--- Api/Modules/Files/Services/Repository/FilesRepository.cs
+++ Api/Modules/Files/Services/Repository/FilesRepository.cs
@@ -14,28 +14,28 @@ namespace Api.Modules.Files.Services.Repository;
 /// <inheritdoc cref="IFilesRepository" />
 public class FilesRepository : IFilesRepository, ITransientService
 {
-	private readonly IDatabaseConnection databaseConnection;
+    private readonly IDatabaseConnection databaseConnection;
 
-	/// <summary>
-	/// Creates a new instance of the <see cref="FilesRepository"/> class.
-	/// </summary>
-	public FilesRepository(IDatabaseConnection databaseConnection)
-	{
-		this.databaseConnection = databaseConnection;
-	}
+    /// <summary>
+    /// Creates a new instance of the <see cref="FilesRepository"/> class.
+    /// </summary>
+    public FilesRepository(IDatabaseConnection databaseConnection)
+    {
+        this.databaseConnection = databaseConnection;
+    }
 
-	/// <inheritdoc />
+    /// <inheritdoc />
     public async Task<List<FileTreeViewModel>> GetTreeAsync(ulong parentId = 0, string tablePrefix = "")
     {
-	    await databaseConnection.EnsureOpenConnectionForReadingAsync();
-	    databaseConnection.AddParameter("parentId", parentId);
+        await databaseConnection.EnsureOpenConnectionForReadingAsync();
+        databaseConnection.AddParameter("parentId", parentId);
 
         var query = $@"SELECT
-	id,
-	file_name AS name,
-	content_type AS contentType,
-	FALSE AS isDirectory,
-	FALSE AS hasChildren,
+    id,
+    file_name AS name,
+    content_type AS contentType,
+    FALSE AS isDirectory,
+    FALSE AS hasChildren,
     property_name AS propertyName,
     item_id AS itemId,
     CASE
@@ -50,13 +50,13 @@ WHERE item_id = ?parentId
 UNION ALL
 
 SELECT
-	item.id,
-	item.title AS name,
-	'' AS contentType,
-	TRUE AS isDirectory,
-	IF(COUNT(DISTINCT subItem.id) > 0 OR COUNT(DISTINCT file.id) > 0, TRUE, FALSE) AS hasChildren,
+    item.id,
+    item.title AS name,
+    '' AS contentType,
+    TRUE AS isDirectory,
+    IF(COUNT(DISTINCT subItem.id) > 0 OR COUNT(DISTINCT file.id) > 0, TRUE, FALSE) AS hasChildren,
     '' AS propertyName,
-	item.id AS itemId,
+    item.id AS itemId,
     '{Constants.ClosedDirectoryIconClass}' AS spriteCssClass,
     '' AS html
 FROM {tablePrefix}{WiserTableNames.WiserItem} AS item
@@ -71,18 +71,18 @@ ORDER BY isDirectory DESC, name ASC";
         var dataTable = await databaseConnection.GetAsync(query);
         return dataTable.Rows.Cast<DataRow>().Select(dataRow =>
         {
-	        var model = new FileTreeViewModel();
-	        model.Id = Convert.ToUInt64(dataRow["id"]);
-	        model.Name = dataRow.Field<string>("name");
-	        model.IsDirectory = Convert.ToBoolean(dataRow["isDirectory"]);
-	        model.HasChildren = Convert.ToBoolean(dataRow["hasChildren"]);
-	        model.CollapsedSpriteCssClass = dataRow.Field<string>("spriteCssClass");
-	        model.ExpandedSpriteCssClass = Constants.OpenedDirectoryIconClass;
-	        model.Html = dataRow.Field<string>("html");
-	        model.PropertyName = dataRow.Field<string>("propertyName");
-	        model.ItemId = dataRow.Field<ulong>("itemId");
-	        model.ContentType = dataRow.Field<string>("contentType");
-	        return model;
+            var model = new FileTreeViewModel();
+            model.Id = Convert.ToUInt64(dataRow["id"]);
+            model.Name = dataRow.Field<string>("name");
+            model.IsDirectory = Convert.ToBoolean(dataRow["isDirectory"]);
+            model.HasChildren = Convert.ToBoolean(dataRow["hasChildren"]);
+            model.CollapsedSpriteCssClass = dataRow.Field<string>("spriteCssClass");
+            model.ExpandedSpriteCssClass = Constants.OpenedDirectoryIconClass;
+            model.Html = dataRow.Field<string>("html");
+            model.PropertyName = dataRow.Field<string>("propertyName");
+            model.ItemId = dataRow.Field<ulong>("itemId");
+            model.ContentType = dataRow.Field<string>("contentType");
+            return model;
         }).ToList();
     }
 }
