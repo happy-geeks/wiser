10c5f7826f833f8969132c29d42c8a5d80d9d335
Mike van Mourik <130973326+MikeVanMourik@users.noreply.github.com>
2023-07-18 10:10:47 +0200

    add styled output api endpoint (#299)

---
+++ Api/Modules/Queries/Interfaces/IStyledOutputService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Api.Core.Services;
+using Newtonsoft.Json.Linq;
+
+namespace Api.Modules.Queries.Interfaces
+{
+    /// <summary>
+    /// Service for getting styled output from Wiser
+    /// </summary>
+    public interface IStyledOutputService
+    {
+        /// <summary>
+        /// Find a styled output in the wiser_styled_output table and returns the result
+        /// </summary>
+        /// <param name="id">The ID from wiser_styled_output.</param>
+        /// <param name="parameters">The parameters to set before executing the styled output.</param>
+        /// <param name="stripNewlinesAndTabs">replaces \r\n \n and \t when encountered in the format.</param>
+        /// <returns>The results of the query .</returns>
+        /// 
+        Task<ServiceResult<JToken>> GetStyledOutputResultJsonAsync(ClaimsIdentity identity, int id, List<KeyValuePair<string, object>> parameters, bool stripNewlinesAndTabs);
+    }
+}
