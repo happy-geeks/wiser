596e1b77317869c93b320c79eebc1f388bc33a12
HappyMaarten <144103965+HappyMaarten@users.noreply.github.com>
2023-10-05 16:37:59 +0200

    Fixed brackets according to coding style

--- Api/Modules/Items/FieldTemplates/sub-entities-grid.js
+++ Api/Modules/Items/FieldTemplates/sub-entities-grid.js
@@ -229,32 +229,32 @@ if (customQueryGrid) {
     }
 }
     
-function mergeFilterData(data, newData)
-{
+function mergeFilterData(data, newData){
     // if grid had no filter data yet, the newData is all we have!
-    if (data === undefined)
+    if (data === undefined) {
         return newData;
+    }
     
     // newData.filters is usually length 1, but it doesn't hurt to support >1
     newData.filters.forEach(newFilter => {
         let i = data.filters.findIndex((f) => f.field === newFilter.field);
         
         // if the existing data already has a filter configured for this field, we are either removing it or changing it
-        if (i >= 0)
-        {
+        if (i >= 0) {
             // we are removing this filter
-            if (newFilter.operator == null) 
+            if (newFilter.operator == null) {
                 data.filters.splice(i, 1);
+            }
             // it exists and need to be changed
-            else 
-            {
+            else {
                 data.filters[i].operator = newFilter.operator;
                 data.filters[i].value = newFilter.value;
             }
         }
         // existing data does not have this filter, add it (if it's not a removal somehow)
-        else if (newFilter.operator != null)
+        else if (newFilter.operator != null) {
             data.filters.push(newFilter);
+        }
     });
     return data;
 }
@@ -890,10 +890,10 @@ async function generateGrid(data, model, columns) {
         filter: function (event) {            
             if (options.keepFiltersState !== false) {
                 try {
-                    if (event.field == null) // manual event trigger, all filters were removed
+                    if (event.field == null) { // manual event trigger, all filters were removed
                         dynamicItems.grids.saveGridViewState("sub_entities_grid_filters_{propertyId}", "{}");
-                    else
-                    {
+                    }
+                    else {
                         let mergedData = event.filter != null ?
                             mergeFilterData(event.sender.dataSource.filter(), event.filter) : // filter was added
                             mergeFilterData(event.sender.dataSource.filter(), { "filters": [{ "field": event.field, "operator": null }]}); // filter was removed
