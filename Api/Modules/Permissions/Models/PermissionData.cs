148f74bdf21388ca19203d9ac2a9b7bb0a743177
Marco Ketelaars <MKet@users.noreply.github.com>
2023-12-14 14:22:20 +0100

    Query permissions (#400)
    * Rewrite module permissions tabs to use permission controller and service and add query tab in this style
    
    * Refactor role permission grid
    
    * Add permission data model which was missing in previous commit
    
    * Renamed PermissionSubject to PermissionSubjects and changed NotImplementedException to ArgumentOutOfRangeException
    
    * Some minor code improvements.
    
    ---------
    
    Co-authored-by: Gilian <giliankeulens@happyhorizon.com>

---
+++ Api/Modules/Permissions/Models/PermissionData.cs
@@ -0,0 +1,32 @@
+namespace Api.Modules.Permissions.Models;
+
+/// <summary>
+/// Model containing permission data for a role
+/// </summary>
+public class PermissionData
+{
+    /// <summary>
+    /// Gets or sets the id of the role the permission is set for
+    /// </summary>
+    public int RoleId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the role the permission is set for
+    /// </summary>
+    public string RoleName { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the access rights the role has for the subject
+    /// </summary>
+    public int Permission { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the id of the object the permission is set for
+    /// </summary>
+    public int ObjectId { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the name of the object the permission is set for
+    /// </summary>
+    public string ObjectName { get; set; }
+}
