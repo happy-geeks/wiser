31fea91d20f2a20699767465025d715db87cb759
Gilian <giliankeulens@happyhorizon.com>
2024-07-09 16:43:52 +0200

    Refactored some code so that modulesService has less side effects. The new controller still needs to be called from javascript now.

---
+++ Api/Core/Interfaces/IWiserDatabaseHelpersService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace Api.Core.Interfaces;
+
+/// <summary>
+/// A service that provides helper methods for the Wiser database.
+/// </summary>
+public interface IWiserDatabaseHelpersService
+{
+    /// <summary>
+    /// Do database migrations for the tenant database, of the currently authenticated user, to keep all tables and data up-to-date.
+    /// </summary>
+    Task DoDatabaseMigrationsForTenantAsync();
+
+    /// <summary>
+    /// Do database migrations for the main database, to keep all tables and data up-to-date.
+    /// </summary>
+    Task DoDatabaseMigrationsForMainDatabaseAsync();
+}
