fef3ddeec1bcd377e5dc0956de65c56ffb2e94ec
Gilian <giliankeulens@happyhorizon.com>
2024-07-11 16:08:18 +0200

    Found a new way that almost works, with a js library

---
+++ Api/Core/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Core.Controllers;
+
+public class ErrorController : Controller
+{
+    private readonly IIdentityServerInteractionService interactionService;
+
+    public ErrorController(IIdentityServerInteractionService interactionService)
+    {
+        this.interactionService = interactionService;
+    }
+
+    [HttpGet("/home/error")]
+    public async Task<IActionResult> Error([FromQuery] string errorId)
+    {
+        var error = await interactionService.GetErrorContextAsync(errorId);
+
+        return new JsonResult(error);
+    }
+}
