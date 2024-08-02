c924fdf66acd254638538f096893ae12ca70007d
Mike van Mourik <130973326+MikeVanMourik@users.noreply.github.com>
2024-08-02 08:53:34 +0200

    image upload js changes

--- Api/Modules/Items/FieldTemplates/image-upload.js
+++ Api/Modules/Items/FieldTemplates/image-upload.js
@@ -4,7 +4,7 @@ const readonly = {readonly};
 const  initialFiles = {initialFiles};
 
 if (initialFiles && initialFiles.length > 0) {
-    for (var i = 0; i < initialFiles.length; i++) {
+    for (let i = 0; i < initialFiles.length; i++) {
         initialFiles[i].readonly = readonly;
         initialFiles[i].entityType = "{entityType}";
         initialFiles[i].linkType = "{linkType}";
@@ -59,31 +59,31 @@ container.find(".imagesContainer").kendoSortable({
     cursor: "move",
     autoScroll: true,
     container: "#container_{propertyIdWithSuffix} .imagesContainer",
-    hint: function (element) {
+    hint: (element) => {
         return element.clone().addClass("hint");
     },
-    placeholder: function (element) {
+    placeholder: (element) => {
         return element.clone().addClass("k-state-hover").css("opacity", 0.65);
     },
-    change: function (event) {
+    change: async (event) => {
         // Kendo starts ordering with 0, but wiser starts with 1.
         const oldIndex = event.oldIndex + 1;
         const newIndex = event.newIndex + 1;
         const fileId = event.item.data("imageId");
         const propertyName = container.data("propertyName");
 
-        Wiser.api({
-            method: "PUT",
-            contentType: "application/json",
-            dataType: "json",
-            url: `${dynamicItems.settings.wiserApiRoot}items/{itemId}/files/${fileId}/ordering?previousPosition=${oldIndex}&newPosition=${newIndex}&propertyName=${encodeURIComponent(propertyName)}&itemLinkId={itemLinkId}&entityType=${encodeURIComponent("{entityType}")}&linkType={linkType}`
-        }).then(function (dataResult) {
-        }).catch(function (jqXHR, textStatus, errorThrown) {
+        try {
+            let result = Wiser.api({
+                method: "PUT",
+                contentType: "application/json",
+                dataType: "json",
+                url: `${dynamicItems.settings.wiserApiRoot}items/{itemId}/files/${fileId}/ordering?previousPosition=${oldIndex}&newPosition=${newIndex}&propertyName=${encodeURIComponent(propertyName)}&itemLinkId={itemLinkId}&entityType=${encodeURIComponent("{entityType}")}&linkType={linkType}`
+            });
+        } catch (result) {
             console.error("Update file order error - {title}", jqXHR, textStatus, errorThrown);
             kendo.alert("Er is iets fout gegaan tijdens het aanpassen van de volgorde. Probeer het a.u.b. nogmaals of neem contact op met ons.");
-        });
+        }
     }
-});
-
+    });
 {customScript}
 })();
