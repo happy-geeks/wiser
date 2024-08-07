7f6f3a359604937afd336d394f284736d53f2185
Gilian <giliankeulens@happyhorizon.com>
2024-08-07 14:15:34 +0200

    Added wiser_history to tables to keep updated.

--- Api/Modules/Modules/Services/ModulesService.cs
+++ Api/Modules/Modules/Services/ModulesService.cs
@@ -10,14 +10,13 @@ using System.Web;
 using Api.Core.Helpers;
 using Api.Core.Interfaces;
 using Api.Core.Services;
-using Api.Modules.Tenants.Interfaces;
 using Api.Modules.Grids.Interfaces;
 using Api.Modules.Kendo.Models;
 using Api.Modules.Modules.Interfaces;
 using Api.Modules.Modules.Models;
+using Api.Modules.Tenants.Interfaces;
 using GeeksCoreLibrary.Core.DependencyInjection.Interfaces;
 using GeeksCoreLibrary.Core.Enums;
-using GeeksCoreLibrary.Core.Extensions;
 using GeeksCoreLibrary.Core.Interfaces;
 using GeeksCoreLibrary.Core.Models;
 using GeeksCoreLibrary.Modules.Databases.Interfaces;
@@ -48,7 +47,7 @@ namespace Api.Modules.Modules.Services
         private readonly ILogger<ModulesService> logger;
         private readonly IDatabaseHelpersService databaseHelpersService;
         private readonly ICsvService csvService;
-        
+
         private const string DefaultModulesGroupName = "Overig";
         private const string PinnedModulesGroupName = "Vastgepind";
 
@@ -137,6 +136,7 @@ namespace Api.Modules.Modules.Services
                 WiserTableNames.WiserCommunication,
                 WiserTableNames.WiserStyledOutput,
                 WiserTableNames.WiserParentUpdates,
+                WiserTableNames.WiserHistory,
                 GeeksCoreLibrary.Modules.Databases.Models.Constants.DatabaseConnectionLogTableName
             });
 
@@ -750,7 +750,7 @@ SELECT @newID;";
             var result = excelService.JsonArrayToExcel(newData);
             return new ServiceResult<byte[]>(result);
         }
-        
+
         /// <inheritdoc />
         public async Task<ServiceResult<byte[]>> ExportToCsvAsync(int id, ClaimsIdentity identity, char separator)
         {
@@ -763,7 +763,7 @@ SELECT @newID;";
                     StatusCode = gridResult.StatusCode
                 };
             }
-            
+
             var newData = new JArray();
             var data = gridResult.ModelObject.Data;
             var columns = gridResult.ModelObject.Columns;
