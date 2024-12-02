c8310de6041740d2bb5d365710daee23a6c3b7fe
Marco Ketelaars <marcoketelaars@happyhorizon.com>
2024-12-02 16:34:42 +0100

    Add check for unauthorized for tenant migration

--- FrontEnd/Core/Scripts/shared/databases.service.js
+++ FrontEnd/Core/Scripts/shared/databases.service.js
@@ -5,8 +5,12 @@ export default class DatabasesService extends BaseService {
         try {
             await this.base.api.put(`/api/v3/database/tenant-migrations`);
         } catch (exception) {
-            console.error("Error while executing database migrations", exception);
-            alert("Er is iets fout gegaan tijdens het uitvoeren van de database migraties. Ververs a.u.b. de pagina om het opnieuw te proberen, of neem contact op met de beheerder.");
+            if (exception.response.status === 401) {
+                console.warn("User is unauthorized to do tenant migrations.", exception);
+            } else {
+                console.error("Error while executing database migrations", exception);
+                alert("Er is iets fout gegaan tijdens het uitvoeren van de database migraties. Ververs a.u.b. de pagina om het opnieuw te proberen, of neem contact op met de beheerder.");
+            }
         }
     }
 }
