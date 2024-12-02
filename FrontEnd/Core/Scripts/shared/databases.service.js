5f4345dbe4171b6f9e0bb1372d66cae167d680ab
Marco Ketelaars <marcoketelaars@happyhorizon.com>
2024-12-02 16:47:14 +0100

    Change tenant migration to database migrations for consistency

--- FrontEnd/Core/Scripts/shared/databases.service.js
+++ FrontEnd/Core/Scripts/shared/databases.service.js
@@ -6,7 +6,7 @@ export default class DatabasesService extends BaseService {
             await this.base.api.put(`/api/v3/database/tenant-migrations`);
         } catch (exception) {
             if (exception.response.status === 401) {
-                console.warn("User is unauthorized to do tenant migrations.", exception);
+                console.warn("User is unauthorized to do database migrations.", exception);
             } else {
                 console.error("Error while executing database migrations", exception);
                 alert("Er is iets fout gegaan tijdens het uitvoeren van de database migraties. Ververs a.u.b. de pagina om het opnieuw te proberen, of neem contact op met de beheerder.");
