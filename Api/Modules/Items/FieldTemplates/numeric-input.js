02ae59b6525cc58a414d4244e5577811a981ac8e
Mike van Mourik <130973326+MikeVanMourik@users.noreply.github.com>
2024-12-11 16:25:07 +0100

    perform javascript improvements/cleanup in numeric-input.js

--- Api/Modules/Items/FieldTemplates/numeric-input.js
+++ Api/Modules/Items/FieldTemplates/numeric-input.js
@@ -1,10 +1,11 @@
-﻿(function() {
-    var options = $.extend({
+﻿(() => {
+    let options = $.extend({
         culture: "nl-NL"
     }, {options});
 
-    var field = $("#field_{propertyIdWithSuffix}");
-    var kendoComponent = field.kendoNumericTextBox($.extend({change: window.dynamicItems.fields.onFieldValueChange.bind(window.dynamicItems.fields)}, {options})).data("kendoNumericTextBox");
+    let field = $("#field_{propertyIdWithSuffix}");
+    
+    let kendoComponent = field.kendoNumericTextBox($.extend({change: window.dynamicItems.fields.onFieldValueChange.bind(window.dynamicItems.fields)}, {options})).data("kendoNumericTextBox");
 
     if (options.saveOnEnter) {
         field.keypress(function(event) {
