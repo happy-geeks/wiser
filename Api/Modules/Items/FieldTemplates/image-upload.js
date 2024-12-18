e7b124201bd1269dff37ba94f2b72430f6378018
Mike van Mourik <130973326+MikeVanMourik@users.noreply.github.com>
2024-12-18 11:05:26 +0100

    perform javascript improvements/cleanup in ImageUpload.js (#796)
    * perform javascript improvements/cleanup in ImageUpload.js
    
    * fixes
    
    * fixes
    
    * address concerns by respected coworkers

--- Api/Modules/Items/FieldTemplates/image-upload.js
+++ Api/Modules/Items/FieldTemplates/image-upload.js
@@ -1,32 +1,44 @@
-﻿(function() {
-var container = $("#container_{propertyIdWithSuffix}");
-var readonly = {readonly};
-var initialFiles = {initialFiles};
+﻿(() => {
+let container = $("#container_{propertyIdWithSuffix}");
+let readonly = {readonly};
+let initialFiles = {initialFiles};
+
 if (initialFiles && initialFiles.length > 0) {
-    for (var i = 0; i < initialFiles.length; i++) {
+    for (let i = 0; i < initialFiles.length; i++) {
         initialFiles[i].readonly = readonly;
         initialFiles[i].entityType = "{entityType}";
         initialFiles[i].linkType = "{linkType}";
     }
 }
 
-var options = {options};
+let options = {options};
 
 options = $.extend({
     async: {
-        saveUrl: window.dynamicItems.settings.wiserApiRoot + "items/{itemIdEncrypted}/upload?propertyName=" + encodeURIComponent("{propertyName}") + "&itemLinkId={itemLinkId}&useTinyPng=" + (options.useTinyPng === true).toString() + "&useCloudFlare=" + (options.useCloudFlare === true).toString() + "&entityType=" + encodeURIComponent("{entityType}") + "&linkType={linkType}",
+        saveUrl: `${window.dynamicItems.settings.wiserApiRoot}"items/{itemIdEncrypted}/upload?propertyName="${encodeURIComponent("{propertyName}")} &itemLinkId={itemLinkId}&useTinyPng=${(options.useTinyPng === true).toString()}&useCloudFlare="${(options.useCloudFlare === true).toString()}"&entityType=${encodeURIComponent("{entityType}")}&linkType={linkType}`,
         withCredentials: false,
         removeUrl: "remove"
     },
     validation: {
-        allowedExtensions: [".jpg", ".jpeg", ".png", ".bmp", ".gif", ".svg", ".webp", ".tif", ".tiff"]
+        allowedExtensions: [
+            ".bmp",
+            ".gif",
+            ".jpg", 
+            ".jpeg", 
+            ".png",
+            ".svg",
+            ".tif",
+            ".tiff",
+            ".webp"
+        ]
     },
     files: initialFiles,
     upload: (e) => {
         let xhr = e.XMLHttpRequest;
         if (xhr) {
             xhr.addEventListener("readystatechange", (e) => {
-                if (xhr.readyState === 1 /* OPENED */) {
+                /* OPENED */
+                if (xhr.readyState === 1) {
                     xhr.setRequestHeader("authorization", `Bearer ${localStorage.getItem("accessToken")}`);
                 }
             });
@@ -44,8 +56,8 @@ if (window.dynamicItems.fields.onFileUploadError) {
     options.error = window.dynamicItems.fields.onFileUploadError.bind(window.dynamicItems.fields);
 }
 
-var field = $("#field_{propertyIdWithSuffix}");
-var kendoComponent = field.kendoUpload(options).data("kendoUpload");
+let field = $("#field_{propertyIdWithSuffix}");
+let kendoComponent = field.kendoUpload(options).data("kendoUpload");
 
 $("#images_{propertyIdWithSuffix}").html(kendo.render(kendo.template($("#uploaderTemplate").html()), initialFiles));
 
@@ -58,29 +70,32 @@ container.find(".imagesContainer").kendoSortable({
     cursor: "move",
     autoScroll: true,
     container: "#container_{propertyIdWithSuffix} .imagesContainer",
-    hint: function (element) {
+    hint: (element) => {
         return element.clone().addClass("hint");
     },
-    placeholder: function (element) {
+    placeholder: (element) => {
         return element.clone().addClass("k-state-hover").css("opacity", 0.65);
     },
-    change: function (event) {
+    change: async (event) => {
         // Kendo starts ordering with 0, but wiser starts with 1.
         const oldIndex = event.oldIndex + 1;
         const newIndex = event.newIndex + 1;
         const fileId = event.item.data("imageId");
         const propertyName = container.data("propertyName");
 
-        Wiser.api({
-            method: "PUT",
-            contentType: "application/json",
-            dataType: "json",
-            url: `${dynamicItems.settings.wiserApiRoot}items/{itemId}/files/${fileId}/ordering?previousPosition=${oldIndex}&newPosition=${newIndex}&propertyName=${encodeURIComponent(propertyName)}&itemLinkId={itemLinkId}&entityType=${encodeURIComponent("{entityType}")}&linkType={linkType}`
-        }).then(function (dataResult) {
-        }).catch(function (jqXHR, textStatus, errorThrown) {
-            console.error("Update file order error - {title}", jqXHR, textStatus, errorThrown);
+        try {
+            let apiResult = await Wiser.api({
+                method: "PUT",
+                contentType: "application/json",
+                dataType: "json",
+                url: `${dynamicItems.settings.wiserApiRoot}items/{itemId}/files/${fileId}/ordering?previousPosition=${oldIndex}&newPosition=${newIndex}&propertyName=${encodeURIComponent(propertyName)}&itemLinkId={itemLinkId}&entityType=${encodeURIComponent("{entityType}")}&linkType={linkType}`
+            });
+        }
+        catch (exception)
+        {
+            console.error("Update file order error - {title}", exception);
             kendo.alert("Er is iets fout gegaan tijdens het aanpassen van de volgorde. Probeer het a.u.b. nogmaals of neem contact op met ons.");
-        });
+        }
     }
 });
 
