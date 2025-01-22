1b493a2048cda48087ef0bd8a04bf9ec445feb8e
Mike van Mourik <130973326+MikeVanMourik@users.noreply.github.com>
2025-01-22 10:26:03 +0100

    perform javascript improvements/cleanup in radiobutton (#825)
    * perform javascript improvements/cleanup in radiobutton
    
    * convert .then to await function
    
    * remove empty line

--- Api/Modules/Items/FieldTemplates/radiobutton.js
+++ Api/Modules/Items/FieldTemplates/radiobutton.js
@@ -1,28 +1,32 @@
-﻿(function() {
-    Wiser.api({
-        url: dynamicItems.settings.wiserApiRoot + "items/" + encodeURIComponent("{itemIdEncrypted}") + "/action-button/{propertyId}",
+﻿(async () => {
+    let results =  await Wiser.api({
+        url: `${dynamicItems.settings.wiserApiRoot}items/${encodeURIComponent("{itemIdEncrypted}")}/action-button/{propertyId}`,
         method: "POST"
-    }).then(function(results) {
-        var field = $("#field_{propertyIdWithSuffix}");
-        var defaultValue = field.data("defaultValue");
-
-        for (var i = 0; i < results.otherData.length; i++) {
-            var result = results.otherData[i];
-            var label = $("<label>").addClass("radio");
-            var input = $("<input>")
-                .attr("type", "radio")
-                .attr("name", field.data("name"))
-                .attr("value", result.id)
-                .prop("required", field.data("required") === "required")
-                .attr("pattern", field.data("pattern"))
-                .prop("checked", defaultValue == result.id)
-                .prop("disabled", {readonly})
-                .appendTo(label);
-            var span = $("<span>").addClass("label").text(result.name).appendTo(label);
-            input.change(window.dynamicItems.fields.onFieldValueChange.bind(window.dynamicItems.fields));
-            field.append(label);
-        }
-
-        {customScript}
     });
+    
+    let field = $("#field_{propertyIdWithSuffix}");
+    let defaultValue = field.data("defaultValue");
+
+    for (let i = 0; i < results.otherData.length; i++) {
+        let result = results.otherData[i];
+        
+        let label = $("<label>").addClass("radio");
+        
+        let input = $("<input>")
+            .attr("type", "radio")
+            .attr("name", field.data("name"))
+            .attr("value", result.id)
+            .prop("required", field.data("required") === "required")
+            .attr("pattern", field.data("pattern"))
+            .prop("checked", defaultValue == result.id)
+            .prop("disabled", {readonly})
+            .appendTo(label);
+        
+        let span = $("<span>").addClass("label").text(result.name).appendTo(label);
+        
+        input.change(window.dynamicItems.fields.onFieldValueChange.bind(window.dynamicItems.fields));
+        field.append(label);
+    }
+
+    {customScript}
 })();
