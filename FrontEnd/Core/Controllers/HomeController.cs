874b583bc38631e131d75b0ee4c8e05e74831c48
Gilian <giliankeulens@happyhorizon.com>
2025-04-01 14:10:12 +0200

    Some code cleanup.

--- FrontEnd/Core/Controllers/HomeController.cs
+++ FrontEnd/Core/Controllers/HomeController.cs
@@ -2,28 +2,23 @@
 using System.IO;
 using System.Linq;
 using FrontEnd.Core.Interfaces;
-using FrontEnd.Core.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 
 namespace FrontEnd.Core.Controllers;
 
-public class HomeController(IOptions<FrontEndSettings> frontEndSettings, IBaseService baseService, IWebHostEnvironment webHostEnvironment)
-    : Controller
+public class HomeController(IBaseService baseService, IWebHostEnvironment webHostEnvironment) : Controller
 {
-    private readonly FrontEndSettings frontEndSettings = frontEndSettings.Value;
-
     public IActionResult Index()
     {
         var viewModel = baseService.CreateBaseViewModel();
-            
-        var partnerStylesDirectory = new DirectoryInfo(Path.Combine(webHostEnvironment.ContentRootPath, @"Core/Css/partner"));
+
+        var partnerStylesDirectory = new DirectoryInfo(Path.Combine(webHostEnvironment.ContentRootPath, "Core/Css/partner"));
         if (partnerStylesDirectory.Exists)
         {
             viewModel.LoadPartnerStyle = partnerStylesDirectory.GetFiles("*.css").Any(f => Path.GetFileNameWithoutExtension(f.Name).Equals(viewModel.SubDomain, StringComparison.OrdinalIgnoreCase));
         }
-            
+
         return View(viewModel);
     }
 }
