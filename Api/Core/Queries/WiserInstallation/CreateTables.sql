92508a2a5c21be0854792ab3a5d42b92f5750ebd
Mike van Mourik <130973326+MikeVanMourik@users.noreply.github.com>
2025-04-28 10:36:21 +0200

    correct indexes for product api table

--- Api/Core/Queries/WiserInstallation/CreateTables.sql
+++ Api/Core/Queries/WiserInstallation/CreateTables.sql
@@ -936,8 +936,8 @@ CREATE TABLE IF NOT EXISTS `wiser_products_api`  (
     `hash` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
     `refresh_date` datetime NULL DEFAULT NULL,
     PRIMARY KEY (`id`) USING BTREE
-    INDEX `idx_removed`(`removed` ASC) USING BTREE,
-    INDEX `template_id`(`wiser_id` ASC) USING BTREE,
+    UNIQUE INDEX `idx_unique`(`wiser_id` ASC, `version` ASC) USING BTREE,
+    INDEX `idx_wiser_id`(`wiser_id` ASC, `removed` ASC) USING BTREE,
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
