26dd9e8c651e7edcb19d38cd5b6c62d834eb9d93
Mike van Mourik <130973326+MikeVanMourik@users.noreply.github.com>
2025-04-29 09:41:48 +0200

    use string interpolation instead of verbatim where possible, comment fixes.

--- Api/Modules/Products/Services/ProductsService.cs
+++ Api/Modules/Products/Services/ProductsService.cs
@@ -37,7 +37,7 @@ public class ProductsService(
     : IProductsService, IScopedService
 {
     private const string ProductApiPropertyTabName = "Product Api";
-    
+
     /// <inheritdoc />
     public async Task<ServiceResult<JToken>> GetProduct(ClaimsIdentity identity, ulong wiserId)
     {
@@ -49,16 +49,14 @@ public class ProductsService(
         clientDatabaseConnection.ClearParameters();
         clientDatabaseConnection.AddParameter("id", wiserId);
 
-        var selectQuery =
-            $"SELECT `output` FROM {WiserTableNames.WiserProductsApi} item WHERE item.wiser_id = ?id  ORDER BY `version` DESC LIMIT 1";
+        var selectQuery = $"SELECT `output` FROM {WiserTableNames.WiserProductsApi} item WHERE item.wiser_id = ?id  ORDER BY `version` DESC LIMIT 1";
 
         var dataTable = await clientDatabaseConnection.GetAsync(selectQuery);
 
         // Check if we have a result, if not we inform the user its not there yet, they should generate it first.(don't lazy load/gen it).
         if (dataTable.Rows.Count == 0)
         {
-            var errorMsg =
-                $"Wiser product api Output for product with id '{wiserId}' does not exist( did you not generate it yet?).";
+            var errorMsg = $"Wiser product api Output for product with id '{wiserId}' does not exist( did you not generate it yet?).";
             logger.LogError(errorMsg);
             return new ServiceResult<JToken>
             {
@@ -77,8 +75,7 @@ public class ProductsService(
         }
         catch
         {
-            var errorMsg =
-                $"Wiser product api Output for product with id '{wiserId}' does not have valid Json content.";
+            var errorMsg = $"Wiser product api Output for product with id '{wiserId}' does not have valid Json content.";
             logger.LogError(errorMsg);
             return new ServiceResult<JToken>
             {
@@ -132,18 +129,18 @@ public class ProductsService(
         clientDatabaseConnection.AddParameter("resultsPerPage", resultsPerPage);
 
         // Make a temp table and store the call the select query into it, use this for a hard join with the product api table.
-        string tempTableQuery = $@"
+        var tempTableQuery = $"""
 DROP temporary table IF EXISTS wiser_temp_{WiserTableNames.WiserProductsApi}_ids;
 CREATE TEMPORARY TABLE wiser_temp_{WiserTableNames.WiserProductsApi}_ids ( wiser_id INT );
 INSERT INTO wiser_temp_{WiserTableNames.WiserProductsApi}_ids (wiser_id)
 {productsQuery}
-";
+""";
         await clientDatabaseConnection.ExecuteAsync(tempTableQuery);
 
         // Now get the actual product api data,
         // Debug note: when testing it is best to remove the hard join (tempids) with the temp table, this will give you all products.
         // Alternatively you can also copy the previous query and run it along with this one to test with the temp table in place.
-        string selectQuery = $@"
+        var selectQuery = $"""
 SELECT wp.wiser_id, wp.output
 FROM {WiserTableNames.WiserProductsApi} wp
 JOIN wiser_temp_{WiserTableNames.WiserProductsApi}_ids tempids ON tempids.wiser_id = wp.wiser_id
@@ -155,7 +152,8 @@ JOIN (
 ON wp.wiser_id = latest.wiser_id 
 AND wp.version = latest.max_version
 WHERE added_on > ?productChangesSince
-LIMIT ?pageOffset , ?resultsPerPage";
+LIMIT ?pageOffset , ?resultsPerPage
+""";
 
         var dataTable = await clientDatabaseConnection.GetAsync(selectQuery);
 
@@ -265,7 +263,7 @@ LIMIT ?pageOffset , ?resultsPerPage";
         var coolDownString =
             ignoreCoolDown ? "" : $"AND apis.refresh_date < DATE_SUB(NOW(), INTERVAL {coolDown} MINUTE) OR apis.refresh_date IS NULL";
 
-        var getApiDataQuery = $@"
+        var getApiDataQuery = $"""
 SELECT 
 item.id AS `wiser_id`,
 apis.hash AS `old_hash`,
@@ -292,7 +290,8 @@ LEFT JOIN {WiserTableNames.WiserProductsApi} apis ON apis.wiser_id = item.id AND
 WHERE item.entity_type = '{productEntityType}' 
   {coolDownString}
   AND item.id IN ({string.Join(",", wiserIds)})
-LIMIT 256";
+LIMIT 256
+""";
         await clientDatabaseConnection.EnsureOpenConnectionForReadingAsync();
         var dataTable = await clientDatabaseConnection.GetAsync(getApiDataQuery);
 
@@ -425,9 +424,10 @@ LIMIT 256";
     /// <param name="currentDateTime">The id of the wiser product we are trying to read.</param>
     private async Task UpdateProductApiEntries(ClaimsIdentity identity, ICollection<ulong> apiEntryIds, string currentDateTime)
     {
-        var query = $@"
-        UPDATE `wiser_products_api` SET `refresh_date` = {currentDateTime}
-        WHERE `wiser_id` IN ( {string.Join(",", apiEntryIds)} )";
+        var query = $"""
+UPDATE `wiser_products_api` SET `refresh_date` = {currentDateTime}
+WHERE `wiser_id` IN ( {string.Join(",", apiEntryIds)} )
+""";
 
         await clientDatabaseConnection.EnsureOpenConnectionForReadingAsync();
         clientDatabaseConnection.ClearParameters();
@@ -460,15 +460,17 @@ LIMIT 256";
             throw new KeyNotFoundException(errorMsg);
         }
 
-        var selectQuery = $@"
+        var selectQuery = $"""
 SELECT id FROM {WiserTableNames.WiserEntityProperty} property 
-WHERE property.tab_name = '{ProductApiPropertyTabName}' AND property.entity_name = '{productEntityType}'";
+WHERE property.tab_name = '{ProductApiPropertyTabName}' AND property.entity_name = '{productEntityType}'
+""";
 
         var dataTable = await clientDatabaseConnection.GetAsync(selectQuery);
 
         if (dataTable.Rows.Count == 0)
         {
             // Select query for the display element.
+            // (Use verbatim strings because have both escaped and evaluated values.)
             var selectQueryForResultDisplay = $@"
 SELECT
 output AS `jsonResult`
@@ -479,6 +481,7 @@ LIMIT 1;
 ";
 
             // We are missing the properties, we need to create them.
+            // (Use verbatim strings because have both escaped and evaluated values.)
             var createPropertyQuery = $@"
 INSERT INTO `wiser_entityproperty` (`label_style`, `label_width`, `module_id`, `entity_name`, `tab_name`, `group_name`, `inputtype`, `display_name`, `property_name`,`ordering`, `default_value`, `width`, `height`, `options`, `explanation`,`data_query`) 
 VALUES ('normal', 0, 620, '{productEntityType}', '{ProductApiPropertyTabName}', '', 'empty', 'results', 'results', 510, '<pre>{{jsonResult}}</pre>', 100, 0, '{{}}', '.', ""{selectQueryForResultDisplay}"")
@@ -523,12 +526,12 @@ ON DUPLICATE KEY UPDATE id=id;
         }
 
         // Let's see if there are any products without details ( we assume that if datasource_type is there all details are saved).
-        var findMissingQuery = $@"
+        var findMissingQuery = $"""
 SELECT item.id AS wiser_id
 FROM {WiserTableNames.WiserItem} `item`
 LEFT JOIN {WiserTableNames.WiserItemDetail} `detail` ON `detail`.key = '{ProductsServiceConstants.ProductPropertyDatasourceType}' AND `detail`.item_id = item.id
 WHERE detail.`id` IS NULL AND item.entity_type = '{productEntityType}' AND item.id IN ({string.Join(",", wiserIds)})
-";
+""";
         var missingDataTable = await clientDatabaseConnection.GetAsync(findMissingQuery);
 
         if (missingDataTable.Rows.Count > 0)
@@ -573,10 +576,11 @@ WHERE detail.`id` IS NULL AND item.entity_type = '{productEntityType}' AND item.
         }
 
         // Fetch the ids of all product api's based on if the datasourcetype is found.
-        var getIds = $@"
+        var getIds = $"""
 SELECT item_id
 FROM wiser_itemdetail
-WHERE `key` = '{ProductsServiceConstants.ProductPropertyDatasourceType}'";
+WHERE `key` = '{ProductsServiceConstants.ProductPropertyDatasourceType}'
+""";
 
         var products = await clientDatabaseConnection.GetAsync(getIds);
 
@@ -610,10 +614,11 @@ WHERE `key` = '{ProductsServiceConstants.ProductPropertyDatasourceType}'";
         clientDatabaseConnection.ClearParameters();
         clientDatabaseConnection.AddParameter("settingName", settingName);
 
-        var selectQuery = $@"
+        var selectQuery = $"""
 SELECT `value` FROM {WiserTableNames.WiserItemDetail} detail 
 JOIN {WiserTableNames.WiserItem} item ON item.id = detail.item_id AND item.entity_type = '{ProductsServiceConstants.SettingsEntityName}' 
-WHERE detail.key = '{settingName}' LIMIT 1";
+WHERE detail.key = '{settingName}' LIMIT 1
+""";
 
         var dataTable = await clientDatabaseConnection.GetAsync(selectQuery);
 
@@ -648,24 +653,29 @@ WHERE detail.key = '{settingName}' LIMIT 1";
         await wiserItemsService.SaveItemDetailAsync(
             new WiserItemDetailModel
             {
-                Key = ProductsServiceConstants.ProductPropertyDatasourceType, Value = globalSettingDatasourceType,
+                Key = ProductsServiceConstants.ProductPropertyDatasourceType,
+                Value = globalSettingDatasourceType,
                 GroupName = "general"
             }, wiserId, entityType: productEntityType, saveHistory: false);
         await wiserItemsService.SaveItemDetailAsync(
             new WiserItemDetailModel
             {
-                Key = ProductsServiceConstants.ProductPropertyQueryId, Value = globalSettingsQueryId, GroupName = "general"
-            }, wiserId, entityType: productEntityType, saveHistory: false);
-        await wiserItemsService.SaveItemDetailAsync(
-            new WiserItemDetailModel
-            {
-                Key = ProductsServiceConstants.ProductPropertyStyledOutputId, Value = globalSettingsStyledOutputId,
+                Key = ProductsServiceConstants.ProductPropertyQueryId,
+                Value = globalSettingsQueryId,
                 GroupName = "general"
             }, wiserId, entityType: productEntityType, saveHistory: false);
         await wiserItemsService.SaveItemDetailAsync(
             new WiserItemDetailModel
             {
-                Key = ProductsServiceConstants.ProductPropertyStatic, Value = globalSettingsStaticText, GroupName = "general"
+                Key = ProductsServiceConstants.ProductPropertyStyledOutputId,
+                Value = globalSettingsStyledOutputId,
+                GroupName = "general"
+            }, wiserId, entityType: productEntityType, saveHistory: false);
+        await wiserItemsService.SaveItemDetailAsync(
+            new WiserItemDetailModel
+            {
+                Key = ProductsServiceConstants.ProductPropertyStatic,
+                Value = globalSettingsStaticText, GroupName = "general"
             }, wiserId, entityType: productEntityType, saveHistory: false);
     }
 
@@ -681,9 +691,10 @@ WHERE detail.key = '{settingName}' LIMIT 1";
         foreach (var item in items)
         {
             insertQuery.Append
-            ($@"
+            ($"""
 INSERT INTO `{WiserTableNames.WiserProductsApi}` (`wiser_id`, `version`, `output`, `added_by`, `hash`,`refresh_date`,`added_on`) 
-VALUES ('{item.WiserId}', '{item.Version}', '{item.Output}', '{item.AddedBy}', '{item.Hash}', '{item.RefreshDate}', '{item.AddedOn}'); ");
+VALUES ('{item.WiserId}', '{item.Version}', '{item.Output}', '{item.AddedBy}', '{item.Hash}', '{item.RefreshDate}', '{item.AddedOn}');
+""");
         }
 
         try
