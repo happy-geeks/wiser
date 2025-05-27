a66abca8bb62f95a2abfd7473e9d9ad1a63ee6bb
Mike van Mourik <130973326+MikeVanMourik@users.noreply.github.com>
2025-05-27 17:05:50 +0200

    add build in logic to styled output

---
+++ Api/Modules/Queries/Exceptions/StyledOutputInvalidStyleOutputpException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Api.Modules.Queries.Exceptions;
+
+/// <summary>
+/// Custom exception for issues with styled output.
+/// </summary>
+public class StyledOutputInvalidStyleOutputpException : Exception
+{
+    /// <summary>
+    /// Create a new <see cref="StyledOutputInvalidStyleOutputpException"/> with only a message/
+    /// </summary>
+    /// <param name="message">The message of the error.</param>
+    public StyledOutputInvalidStyleOutputpException(string message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Create a new <see cref="StyledOutputNoValidQuerySetupException"/> with a message and an inner exception to include an existing exception.
+    /// </summary>
+    /// <param name="message">The message of the error.</param>
+    /// <param name="innerException">The inner exception to be included.</param>
+    public StyledOutputInvalidStyleOutputpException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        var exceptionMessage = InnerException != null ? $"with the following message: {InnerException.Message}" : string.Empty;
+        return $"{base.ToString()}{Environment.NewLine}{Environment.NewLine}StyledOutputInvalidStyleOutputpException Failed {exceptionMessage}";
+    }
+}
